824efb2482f7001c0bfea5d4297b5b4368c3ae0f
Ruhit Abdul-Qahhar <ruhit.abdulqahhar@vodafone.com>
2023-01-05 11:13:28 +0000

    completed 2nd project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.google.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "Ruhit",
+    "lastName": "Abdul-Qahhar"
+  }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        rows="40"
+        cols="40"
+        id="editor"
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text, { breaks: true }) }}
+      ></div>
     </div>
   );
 }
